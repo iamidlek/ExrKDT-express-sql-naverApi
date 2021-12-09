2d7e83390ddd98999bb4ccb215312d54ca9e6a37
iamidlek <iamidlek@naver.com>
2021-12-10 00:58:07 +0900

    feat: make navigate

---
+++ client/src/pages/Router.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Box from "@mui/material/Box";
+import Tab from "@mui/material/Tab";
+import Tabs from "@mui/material/Tabs";
+
+const Router = () => {
+  // history에서 navigate로
+  const navigate = useNavigate();
+
+  // active 스타일을 위한 핸들러
+  const [value, setValue] = useState(0);
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+  return (
+    <Box sx={{ borderBottom: 1, borderColor: "#ccc" }}>
+      <Tabs value={value} onChange={handleChange}>
+        <Tab label="Product" onClick={() => navigate("/")} />
+        <Tab label="Chart" onClick={() => navigate("/chart")} />
+      </Tabs>
+    </Box>
+  );
+};
+
+export default Router;
